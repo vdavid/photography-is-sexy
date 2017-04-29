f110ece8f057b1a0bac0892f9504ffad103f4006
Veszelovszki Dávid <veszelovszki@gmail.com>
2017-04-29 22:37:16 +0200

    Fixed favicon (and removed a lot of files that were not needed for a favicon).

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './Application.jsx';
 
+import './images/favicon.ico';
+
 ReactDOM.render(<Application />, document.getElementById('root'));
